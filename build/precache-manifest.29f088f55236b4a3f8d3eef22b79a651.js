3f66f495b0ccb4f8c2b3d58567f874fcd006cec9
aryaannurseno1 <aaryasenoo@gmail.com>
2019-05-23 10:51:34 +0700

    Hosting

---
+++ build/precache-manifest.29f088f55236b4a3f8d3eef22b79a651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d337ffc35a59f242f3b",
+    "url": "/static/js/main.25d40428.chunk.js"
+  },
+  {
+    "revision": "9f0ba292e64a3d23336a",
+    "url": "/static/js/2.9b449477.chunk.js"
+  },
+  {
+    "revision": "0d337ffc35a59f242f3b",
+    "url": "/static/css/main.17502e55.chunk.css"
+  },
+  {
+    "revision": "65dcfab7bf9f7bb0e06011ce112c1f27",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  BarChart, Bar, XAxis, Legend, LineChart, CartesianGrid, YAxis,  Tooltip, Line
 } from 'recharts';
 
 
@@ -11,7 +11,8 @@ export default class Example extends PureComponent {
     super(props)
     this.state = {
     value: '',
-    data:[]
+    data:[],
+    dataAnalog: [],
     }
   }
   
@@ -24,7 +25,19 @@ export default class Example extends PureComponent {
   handleClick = () => {
 
     const num = this.state.value.split('');
-    if(this.state.value.includes(1) || this.state.value.includes(0) ){
+    const valid = num.map(e =>{
+      if(e.includes(1) || e.includes(0)){
+        return true
+      }
+      else{
+        return false
+      }
+    })
+    if(valid.includes(false)){
+      alert('Bukan angka 1 atau 0')
+      this.setState({ value: '' });
+    }
+    else{
       let data = num.map(e =>{
         if(e == 0){
           return(
@@ -46,12 +59,55 @@ export default class Example extends PureComponent {
           )
         }
       })
+      let dataAnalog = num.map(e =>{
+        this.setState({dataAnalog :{
+          Digital: 0,
+          Label: 0
+
+        }})
+       
+        if(e == 0){
+          return(
+            
+
+              {
+                Digital: Number(e) - 1,
+                Label: 0
+              }
+            
+             )
+  
+        }
+        else{
+          return(
+            {
+              Digital: Number(e),
+              Label: 0
+            }
+    
+          )
+        }
+
+      })
+      console.log(this.state.dataAnalog)
+
+      console.log(dataAnalog)
       this.setState({data: data})
+      this.setState({dataAnalog : dataAnalog}, function () {
+        
+       let abc = this.state.dataAnalog.map((e, els) =>{
+          return e[els]
+        })
+        this.setState({dataReal: abc}, function(){
+          console.log(this.state.dataReal)
+        })
+    })
       this.setState({ value: ''})
-    } else {
-      alert('Bukan angka 1 atau 0')
-      this.setState({ value: '' });
+
+
+
     }
+
  
   }
   
@@ -73,12 +129,34 @@ export default class Example extends PureComponent {
         <Legend />
         <Bar dataKey="Digital" stroke="1" fill="black" />
       </BarChart>
+      
+      <LineChart
+                width={500}
+                height={300}
+                data={
+                  this.state.dataAnalog
+
+                }
+                margin={{
+                  top: 5, right: 30, left: 20, bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="Digital" stroke="#82ca9d" />
+              </LineChart>
             <div>
             <input id="1" value={this.state.value}  onChange={(e) => {this.handleChange(e)}} ref={(input)=> this.myinput = input}/>
             <button onClick={this.handleClick}>CLICK ME</button>
             <p> {this.state.value} </p>
           </div>
+          
           </div>
+
     );
   }
 }
